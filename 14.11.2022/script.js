3aa51b416341fb4c8369bcd3d93b9beccdd2402c
Usu21 <98323022+Usu21@users.noreply.github.com>
2022-11-17 23:51:46 +0500

    Add files via upload

---
+++ 14.11.2022/script.js
@@ -0,0 +1,40 @@
+//1
+
+let massiv = ['13','14','182',];
+console.log(massiv)
+
+//2
+let massiv2 = ['Арбуз','Дыня','Жёлтый арбуз',];
+console.log(massiv2);
+
+//3
+let arr = ['a','b','c','d']; 
+console.log(arr.join('+'))
+//4 
+let massiv3 = ['Арбуз','Дыня','Жёлтый арбуз','13','14','182',];
+console.log(massiv3.length);
+
+//5
+let massiv4 = ['Арбуз','Дыня','Жёлтый арбуз','13','14','182',];
+console.log(massiv4[massiv4.length - 1]);
+
+//6
+let massiv5 = ['a','b','c'];
+massiv5[0] =  1;
+massiv5[1] = -2;
+massiv5[2] = -3;
+console.log(massiv5)
+
+//7
+let massiv6 = [13,14,182,];
+console.log (massiv6.map(i => i+3))
+
+//8
+
+//9
+
+//10
+
+//11
+let massiv10 = ['Арбуз','Дыня','Жёлтый арбуз','13','14','182',];
+console.log(massiv10.reverse());
